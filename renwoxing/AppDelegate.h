f22361cc5cbbdbba525b07414fefb75b5bc4df96
YangBing <imapedia@yangbingdemini.lan>
2016-02-29 10:38:34 +0800

    Initial Commit

---
+++ renwoxing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  renwoxing
+//
+//  Created by YangBing on 16/2/29.
+//  Copyright © 2016年 paixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ renwoxing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  renwoxing
+//
+//  Created by YangBing on 16/2/29.
+//  Copyright © 2016年 paixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
